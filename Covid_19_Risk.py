e1053df27991a9909e7ca86d87acc8d30b4d771d
HarryPotter <llofmejb@gmail.com>
2021-04-24 00:23:20 +0300

    rename

---
+++ Covid_19_Risk.py
@@ -0,0 +1,7 @@
+age = input("Are you a cigarette addict older than 75 years old? : ").capitalize()
+chronic = input("Do you have a severe chronic disease?? : ").capitalize()
+immune = input("Is your immune system too weak? : ").capitalize()
+if (age == "Yes") or (chronic == "Yes") or (immune == "Yes"):
+   print("You are in risky group")
+else:
+    print("You are not in risky group")
--- deneme.py
+++
@@ -1,9 +0,0 @@
-age = input("Are you a cigarette addict older than 75 years old").capitalize()
-chronic = input("Do you have a severe chronic disease?").capitalize()
-immune = input("Is your immune system too weak").capitalize()
-Yes = True
-No = False
-if age and chronic and immune:
-   print("You are in risky group")
-else:
-    print("You are not in risky group")
