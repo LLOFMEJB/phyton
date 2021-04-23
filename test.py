281e9b54ce5aa3776aa92884a7c05f19a6cf1d20
HarryPotter <llofmejb@gmail.com>
2021-04-23 21:29:16 +0300

    name and password

---
+++ test.py
@@ -0,0 +1,6 @@
+name = input("Please write your name: ")
+if name.capitalize() == "Harry":
+    print("Hello, Harry! The password is : W@12")
+else:
+    print(f"Hello, {name.capitalize()}! See you later.")
+    
