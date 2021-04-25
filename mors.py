678dbd0b0ba9575a02f3c8aa0e82c6d60b0c8268
HarryPotter <llofmejb@gmail.com>
2021-04-25 18:46:28 +0300

    geliştirme

--- mors.py
+++ mors.py
@@ -1,23 +1,27 @@
-sentence = (input("Write the sentence to be converted: ")).upper()
-sentence_list = list(sentence)
 morse_codes = {
   'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
   'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
   'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
   'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
-  'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+  'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----', " " : " ",
   '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
   '6': '-....', '7': '--...', '8': '---..', '9': '----.',
   '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
   ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
-  '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'}
-metin = ""
-morse_list = []
-i = 0
-while i < len(sentence_list):
-    morse_list.append(morse_codes[sentence_list[i]])
-    i += 1
-for kelime in morse_list:
-    metin = metin + kelime
-
-print(metin)
+  '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+}
+ 
+# cümle morse koduna çeviriliyor
+sentence = input("lütfen kelime ya da cümle giriniz: ").upper()
+morse = ""
+for i in sentence:
+    morse += morse_codes[i] + " "
+print(morse)        
+# mors kodu cümleye çeviriliyor.
+coded = input("lütfen mors kodunu giriniz: ").split()
+decoded = ""
+for i in coded:
+    for j in morse_codes.keys():
+        if i == morse_codes[j]:
+            decoded += j+" "
+print(decoded)    
