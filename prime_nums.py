967f09acf98db54e07d867b04435829c6b2790bf
HarryPotter <llofmejb@gmail.com>
2021-04-26 19:09:43 +0300

    improvement

--- prime_nums.py
+++ prime_nums.py
@@ -1,9 +1,11 @@
 number = int(input("Write a number: "))
 prime = []
-for i in range(1, 100):
+for i in range(1, number):
     if (number % i) == 0:
         prime.append(i)
 if len(prime) < 3:
     print(number, " is a prime number!")
 else:
     print(number, " is not a prime number!")
+    
+#https://github.com/LLOFMEJB/phyton.git
