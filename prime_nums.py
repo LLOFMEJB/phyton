23fddfab566771be10a0af578d7d60445262033f
HarryPotter <llofmejb@gmail.com>
2021-05-04 01:58:32 +0300

    try

--- prime_nums.py
+++ prime_nums.py
@@ -1,9 +1,9 @@
 number = int(input("Write a number: "))
-prime = []
+prime_list = []
 for i in range(1, number+1):
     if (number % i) == 0:
-        prime.append(i)
-if len(prime) == 2:
+        prime_list.append(i)
+if len(prime_list) == 2:
     print(number, " is a prime number!")
 else:
     print(number, " is not a prime number!")
